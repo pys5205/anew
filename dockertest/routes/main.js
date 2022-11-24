dbcca2113bcd49b62e0a7de9d535172f1f04b932
pys5205 <skypark5205@naver.com>
2022-11-24 00:29:22 +0000

    dockertest

--- dockertest/routes/main.js
+++ dockertest/routes/main.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(morgan('dev'));
 
 var db;
-var databaseUrl = "mongodb+srv://admin:1234@cluster0.uzxwi96.mongodb.net/?retryWrites=true&w=majority";
+var databaseUrl = "mongodb+srv://admin:1234@cluster0.puyp0ow.mongodb.net/?retryWrites=true&w=majority";
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/movie-list', (req, res) => {
+app.get('/food', (req, res) => {
   console.log(mongoClient)
   mongoClient.connect(databaseUrl, function(err, database) {
     if (err != null) {
@@ -22,7 +22,7 @@ app.get('/movie-list', (req, res) => {
       console.log(err);
     } else {
       db = database.db('test');
-      db.collection('movie').find({}, {_id:0, title:1, opening_date:1})
+      db.collection('food').find({}, {_id:0, num:1, name:1})
       .toArray(function(err, result) {
         if (err) throw err;
         console.log('result: ');
@@ -33,16 +33,16 @@ app.get('/movie-list', (req, res) => {
         var template = `
             <table border="1" margin: auto; text-align: center;>
             <tr>
-              <th> 영화제목 </th>
-              <th> 개봉일 </th>
+              <th> 번호 </th>
+              <th> 이름 </th>
             </tr>
         `;
 
 	result.forEach((item) => {
         	template += `
         	<tr>
-        		<th>${item.title}</th>
-        		<th>${item.opening_date}</th>
+        		<th>${item.num}</th>
+        		<th>${item.name}</th>
         	</tr>`
         });
 
