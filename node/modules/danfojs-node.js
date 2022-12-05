3997582caf43560e97c3b5c4d82fe4dd2df0fd80
pys5205 <skypark5205@naver.com>
2022-12-05 05:54:00 +0000

    modules

---
+++ node/modules/danfojs-node.js
@@ -0,0 +1,5 @@
+const dfd = require("danfojs-node")
+
+df = new dfd.Dataframe(res)
+
+df.print();
---
+++ node/modules/pretty-json.js
@@ -0,0 +1,5 @@
+const prettier = require("prettier")
+
+
+
+res.json(prettier.format(JSON.stringify(object)));
